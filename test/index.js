3eb4188a1d855f892ed38a0446ebc3317e0263d1
Neamar <neamar@neamar.fr>
2013-10-13 18:49:43 +0200

    deleteDocument()

--- test/index.js
+++ test/index.js
@@ -1,6 +1,7 @@
 'use strict';
 
 require('should');
+var async = require('async');
 
 var CluestrClient = require('../lib/cluestr.js/index.js');
 
@@ -13,6 +14,20 @@ before(function() {
     if(!process.env[key]) {
       throw new Error("To run this test suite, you need to specify environment variable " + key);
     }
+  });
+});
+
+before(function(done) {
+  // Delete previous tests artifacts
+  var cluestrClient = new CluestrClient(process.env.CLUESTR_ID, process.env.CLUESTR_SECRET);
+  cluestrClient.setAccessToken(process.env.ACCESS_TOKEN);
+  
+  var datas = {
+    identifier: 'test-identifier',
+  };
+  cluestrClient.deleteDocument(datas.identifier, function(err) {
+    // No error checking, since we can't know for sure if old documents are present.
+    done();
   });
 });
 
@@ -51,16 +66,13 @@ describe('sendDocument()', function() {
   });
 
   it('should allow for noHydrate parameter', function(done) {
-    cluestrClient.setAccessToken(process.env.ACCESS_TOKEN);
     cluestrClient.sendDocument({}, false, function(err) {
       err.toString().should.include('identifier');
       done();
     });
   });
 
-  it('should work as documented', function(done) {
-    cluestrClient.setAccessToken(process.env.ACCESS_TOKEN);
-
+  it('should send document', function(done) {
     var datas = {
       identifier: 'test-identifier',
       binary_document_type: 'file',
@@ -79,6 +91,91 @@ describe('sendDocument()', function() {
       document.metadatas.should.eql(datas.metadatas);
 
       done();
+    });
+  });
+
+  it('should update document with same identifier', function(done) {
+    var datas = {
+      identifier: 'test-identifier',
+      binary_document_type: 'file',
+      metadatas: {
+        'foo': 'bar'
+      },
+    };
+
+    var documentId = null;
+    async.series([
+      function(cb) {
+        cluestrClient.sendDocument(datas, function(err, document) {
+          if(err) {
+            throw err;
+          }
+
+          documentId = document.id;
+          cb();
+        });
+      },
+      function(cb) {
+        datas.metadatas.foo2 ="bar2";
+        cluestrClient.sendDocument(datas, function(err, document) {
+          if(err) {
+            throw err;
+          }
+
+          document.should.have.property('id', documentId);
+          document.should.have.property('metadatas');
+          document.metadatas.should.eql(datas.metadatas);
+
+          cb();
+        });
+      }
+    ], done);
+  });
+});
+
+
+describe('deleteDocument()', function() {
+  var cluestrClient = new CluestrClient(process.env.CLUESTR_ID, process.env.CLUESTR_SECRET);
+  cluestrClient.setAccessToken(process.env.ACCESS_TOKEN);
+  
+  var datas = {
+    identifier: 'test-identifier',
+    binary_document_type: 'file',
+    metadatas: {
+      'foo': 'bar'
+    },
+  };
+
+  var documentId = null;
+
+  beforeEach(function(done) {
+    cluestrClient.sendDocument(datas, function(err, document) {
+      if(err) {
+        throw err;
+      }
+
+      documentId = document.id;
+      
+      done();
+    });
+  });
+
+  it('should delete document', function(done) {
+    cluestrClient.deleteDocument(datas.identifier, function(err) {
+      if(err) {
+        throw err;
+      }
+
+      // Sending the same data again should now create a new document
+      cluestrClient.sendDocument(datas, function(err, document) {
+        if(err) {
+          throw err;
+        }
+
+        document.id.should.not.equal(documentId);
+        done();
+      });
+
     });
   });
 });

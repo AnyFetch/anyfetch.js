ab4895778f65e5dccff1cd487566f95696ab2409
Neamar <neamar@neamar.fr>
2013-10-13 17:49:35 +0200

    sendDocument() error tests

--- test/index.js
+++ test/index.js
@@ -8,9 +8,10 @@ var CluestrClient = require('../lib/cluestr.js/index.js');
  * Check environment is set up.
  */
 before(function() {
-  var keys = ['CLUESTR_ID', 'CLUESTR_SECRET'];
+  var keys = ['CLUESTR_ID', 'CLUESTR_SECRET', 'ACCESS_TOKEN'];
   keys.forEach(function(key) {
     if(!process.env[key]) {
+      process.env[key] = 'fake';
       //throw new Error("To run this test suite, you need to specify environment variable " + key);
     }
   });
@@ -28,4 +29,34 @@ describe('getAccessToken()', function() {
       done();
     });
   });
+});
+
+
+describe('sendDocument()', function() {
+  var cluestrClient = new CluestrClient(process.env.CLUESTR_ID, process.env.CLUESTR_SECRET);
+
+  it('should require an accessToken', function(done) {
+    cluestrClient.sendDocument({}, function(err) {
+      err.toString().should.include('accessToken');
+      done();
+    });
+  });
+
+  it('should require an identifier', function(done) {
+    cluestrClient.setAccessToken(process.env.ACCESS_TOKEN);
+    cluestrClient.sendDocument({}, function(err) {
+      err.toString().should.include('identifier');
+      done();
+    });
+  });
+
+  it('should allow for noHydrate parameter', function(done) {
+    cluestrClient.setAccessToken(process.env.ACCESS_TOKEN);
+    cluestrClient.sendDocument({}, false, function(err) {
+      err.toString().should.include('identifier');
+      done();
+    });
+  });
+
+
 });

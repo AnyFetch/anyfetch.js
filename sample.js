2a663aad7cad1e312511fc176ebfb1aca6825958
Merlin Nimier-David <merlin.nimier@gmail.com>
2014-06-06 17:51:38 +0200

    More sample use cases

--- sample.js
+++ sample.js
@@ -9,12 +9,19 @@ Anyfetch.getAccessToken(APP_ID, APP_SECRET, code, function(err, accessToken) {
 	client.getCompany(cb);
 	client.getToken(cb);
 
+	client.getSubcompanyById(123, cb);
+
+	client.postUsers({ endpoint: 'rere' }, cb );
+	client.getDocumentById(123, cb);
+
 	client.updateCompany(cbErr);
 	client.resetCompany(cbErr);
 
 	client.getSubcompanies(cb)
 	client.addSubcompany(user, subcompany, cb)
 	client.deleteSubcompany(subcompanyId, cb)
+	
+	client.deleteSubcompany(subcompanyId, {force: true}, cb)
 
 	client.getDocuments(filters, cb)
 });

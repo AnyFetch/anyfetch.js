a9714fbaa43c4ae7e2ab5775388f5eb5ec09402a
Merlin Nimier-David <merlin.nimier@gmail.com>
2014-06-10 14:09:07 +0200

    Init

---
+++ bin/makeSamples.js
@@ -0,0 +1,7 @@
+'use strict';
+/**
+ * @file Make a call to each endpoint and record the response (code, body, ...)
+ * in JSON files (one file per request).
+ * It is useful to create the mock responses used in testing.
+ * @see http://developers.anyfetch.com/endpoints/
+ */       

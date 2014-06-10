059c9fa5f65978d296c9f532011a9d5288fe611f
Merlin Nimier-David <merlin.nimier@gmail.com>
2014-06-10 16:06:13 +0200

    configuration.js

---
+++ config/configuration.js
@@ -0,0 +1,10 @@
+'use strict';
+
+module.exports = {
+  // Mapping descriptors from JSON files
+  defaultDescriptor: require('../config/json/default-descriptor.json'),
+  apiDescriptors: require('../config/json/api-descriptors.json'),
+  aliases: require('../config/json/aliases.json'),
+
+  apiHost: 'https://api.anyfetch.com'
+};

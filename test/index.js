d82143fa4053eaee57f91bb6861db3bfe0087e57
Merlin Nimier-David <merlin.nimier@gmail.com>
2014-06-11 15:02:07 +0200

    Test stubs

--- test/index.js
+++ test/index.js
@@ -2,4 +2,31 @@
 
 var should = require('should');
 
-var AnyFetchClient = require('../lib/');
+var Anyfetch = require('../lib/index.js');
+
+// Tests to write:
+// getStatus()
+// getCompany()
+// getSubcompanyById()
+// postUser()
+// updateCompany()
+// getDocumentById(123).getRaw()
+// getDocumentById(123, cb)
+
+describe('getStatus', function(){
+  it('should send a request to the correct endpoint', function(done){
+    Anyfetch.getStatus(function(err, res){
+      // TODO: Test value here with should
+      done();
+    });
+  });
+});
+
+describe('getDocumentById', function(){
+  describe('subfunctions', function(){
+    it('should return synchronously an object containing only functions', function(){
+      var ret = Anyfetch.getDocumentById(123);
+      // TODO: Test that `ret` is an object of functions
+    });
+  });
+});

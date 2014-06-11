73e1c01081a33ce89f927b2f129de1731a9306a4
Merlin Nimier-David <merlin.nimier@gmail.com>
2014-06-11 15:57:17 +0200

    Test the request being sent

--- test/index.js
+++ test/index.js
@@ -3,24 +3,52 @@
 var should = require('should');
 
 var Anyfetch = require('../lib/index.js');
+var configuration = require('../config/configuration.js');
 
 // Tests to write:
 // getStatus()
 // getCompany()
 // getSubcompanyById()
 // postUser()
-// updateCompany()
+// postCompanyUpdate()
 // getDocumentById(123).getRaw()
 // getDocumentById(123, cb)
 
-describe('getStatus', function(){
-  it('should send a request to the correct endpoint', function(done){
-    Anyfetch.getStatus(function(err, res){
-      // TODO: Test value here with should
-      done();
+var testEndpoint = function(name){
+
+  describe(name, function(){
+    describe('the request', function(){
+      var expected = configuration.apiDescriptors[name];
+      var r = null;
+
+      before(function(done){
+        // TODO: support id, identifier
+        Anyfetch[name](function(err, res) {
+          r = res;
+          done(err);
+        });
+      });
+
+      it('should use the correct verb', function(){
+        r.req.method.should.equal(expected.verb);
+      });
+      it('should target the correct endpoint', function(){
+        r.req.path.should.equal(expected.endpoint);
+      });
+      it('should have the expected return code', function(){
+        r.res.statusCode.should.equal(expected.expectedStatus);
+      });
     });
   });
-});
+
+};
+
+testEndpoint('getStatus');
+testEndpoint('getIndex');
+testEndpoint('getCompany');
+testEndpoint('postCompanyUpdate');
+testEndpoint('getDocuments');
+testEndpoint('getUsers');
 
 describe('getDocumentById', function(){
   describe('subfunctions', function(){

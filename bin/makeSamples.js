b3f07309125b34030bb10915a1975f6dca1b7f60
Merlin Nimier-David <merlin.nimier@gmail.com>
2014-06-12 18:20:16 +0200

    Save the response from the API in pretty JSON

--- bin/makeSamples.js
+++ bin/makeSamples.js
@@ -6,11 +6,39 @@
  * @see http://developers.anyfetch.com/endpoints/
  */
 
+var fs = require('fs');
+
+var extendDefaults = require('../lib/helpers/extend-defaults.js');
+var filename = require('../lib/helpers/endpoint-filename.js');
+
 var configuration = require('../config/configuration.js');
 var Anyfetch = require('../lib/index.js');
 
-var anyfetch = new Anyfetch(configuration.accessToken);
+var defaultDescriptor = require('../config/json/default-descriptor.json');
+var apiDescriptors = require('../config/json/api-descriptors.json');
+var mocksDirectory = __dirname + '/../lib/test-server/mocks/';
 
-anyfetch.getStatus(function(err, res){
-  console.log(res.body);
+var saveMock = function(endpointConfig, body) {
+  // We'll write pretty JSON
+  var json = JSON.stringify(body, null, 2);
+  var target = filename(endpointConfig) + '.json';
+  fs.writeFile(mocksDirectory + target, json, function(err) {
+    if(err) {
+      throw err;
+    }
+    console.log(target + ' saved.');
+  });
+};
+
+if(!configuration.test.login || !configuration.test.password) {
+  throw new Error('This script requires valid LOGIN and PASSWORD to be set in your env');
+}
+var anyfetch = new Anyfetch(configuration.test.login, configuration.test.password);
+
+var name = 'getStatus';
+anyfetch[name](function(err, res){
+  var config = apiDescriptors[name];
+  extendDefaults(config, defaultDescriptor);
+
+  saveMock(config, res.body);
 });

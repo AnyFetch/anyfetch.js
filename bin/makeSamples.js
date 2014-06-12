cc228f5d0b66b35796b06a367565e335f43fc987
Merlin Nimier-David <merlin.nimier@gmail.com>
2014-06-12 12:04:34 +0200

    Sample usage

--- bin/makeSamples.js
+++ bin/makeSamples.js
@@ -4,4 +4,13 @@
  * in JSON files (one file per request).
  * It is useful to create the mock responses used in testing.
  * @see http://developers.anyfetch.com/endpoints/
- */       
+ */
+
+var configuration = require('../config/configuration.js');
+var Anyfetch = require('../lib/index.js');
+
+var anyfetch = new Anyfetch(configuration.accessToken);
+
+anyfetch.getStatus(function(err, res){
+  console.log(res.body);
+});

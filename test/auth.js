6e7c3898ad5fc28eaf87532d444fce7b6c451563
Merlin Nimier-David <merlin.nimier@gmail.com>
2014-06-23 17:49:39 +0200

    Removed `redirectUri` (was DoorKeeper legacy)

--- test/auth.js
+++ test/auth.js
@@ -12,7 +12,6 @@ describe('<Auth>', function() {
     var appId = configuration.test.fakeAppId;
     var appSecret = configuration.test.fakeAppSecret;
     var code = configuration.test.fakeOAuthCode;
-    var redirectUri = 'http://localhost:5000';
 
     before(function(done) {
       var fakeManagerServer = createFakeManagerServer();
@@ -26,7 +25,7 @@ describe('<Auth>', function() {
     });
 
     it('should err on invalid appId', function(done) {
-      Anyfetch.getAccessToken('wrong_app_id', appSecret, code, redirectUri, function(err) {
+      Anyfetch.getAccessToken('wrong_app_id', appSecret, code, function(err) {
         should(err).be.ok;
         err.message.should.match(/404/);
         done();
@@ -34,7 +33,7 @@ describe('<Auth>', function() {
     });
 
     it('should err on missing code', function(done) {
-      Anyfetch.getAccessToken(appId, appSecret, '', redirectUri, function(err) {
+      Anyfetch.getAccessToken(appId, appSecret, '', function(err) {
         should(err).be.ok;
         err.message.should.match(/409/);
         done();
@@ -42,13 +41,6 @@ describe('<Auth>', function() {
     });
 
     it('should obtain access token', function(done) {
-      Anyfetch.getAccessToken(appId, appSecret, code, redirectUri, function(err, accessToken) {
-        accessToken.should.equal(configuration.test.fakeAccessToken);
-        done(err);
-      });
-    });
-
-    it('should allow `redirect_uri` to be omitted', function(done) {
       Anyfetch.getAccessToken(appId, appSecret, code, function(err, accessToken) {
         accessToken.should.equal(configuration.test.fakeAccessToken);
         done(err);

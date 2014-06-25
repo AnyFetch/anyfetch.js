4219b9ddeb0ee725e260ef591a16c89d44cce012
Merlin Nimier-David <merlin.nimier@gmail.com>
2014-06-25 16:57:50 +0200

    Test for mock `GET /batch`

--- test/mock-server.js
+++ test/mock-server.js
@@ -1,22 +1,33 @@
 'use strict';
 
+var should = require('should');
+
+var Anyfetch = require('../lib/index.js');
 var createMockServer = require('../lib/test-server/index.js');
 var configuration = require('../config/configuration.js');
 
 describe('<Mock server>', function() {
+  var anyfetch = new Anyfetch(configuration.test.login, configuration.test.password);
 
   before(function(done) {
     var server = createMockServer();
     var port = configuration.test.port;
     server.listen(port, function() {
-      console.log('Mock server running on port ' + port);
+      var apiHost = 'http://localhost:' + port;
+      console.log('Mock server running on ' + apiHost);
+      anyfetch.setApiHost(apiHost);
+
       done();
     });
   });
 
-  it('should handle batch calls smartly', function(done) {
-    // TODO
-    done();
+  it('should respond with all the mocks we asked for', function(done) {
+    var pages = ['/document_types', '/providers', '/users', '/company'];
+    anyfetch.getBatch({ pages: pages }, function(err, res) {
+      should(err).not.be.ok;
+      should(res.body).be.ok;
+      res.body.should.have.keys(pages);
+      done();
+    });
   });
-
 });

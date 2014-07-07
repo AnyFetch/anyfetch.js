e75b575396c93cd70a4a8b3e5eebfad0b0861469
Merlin Nimier-David <merlin.nimier@gmail.com>
2014-07-07 15:57:23 +0200

    No need for a standalone fake manager server in tests

--- test/auth.js
+++ test/auth.js
@@ -3,7 +3,6 @@
 var should = require('should');
 
 var AnyFetch = require('../lib/index.js');
-var createFakeManagerServer = require('./helpers/fake-manager-server.js');
 var configuration = require('../config/configuration.js');
 
 describe('<Auth>', function() {
@@ -12,29 +11,15 @@ describe('<Auth>', function() {
     var appId = configuration.test.fakeAppId;
     var appSecret = configuration.test.fakeAppSecret;
     var code = configuration.test.fakeOAuthCode;
-    var fakeManagerServer;
+    var mockServer;
 
     before(function launchServer(done) {
-      fakeManagerServer = createFakeManagerServer();
+      mockServer = AnyFetch.createMockServer();
       var port = configuration.test.managerPort;
 
-      fakeManagerServer.listen(port, function() {
-        console.log('Fake Manager server running on port ' + port);
+      mockServer.listen(port, function() {
+        console.log('Mock server running on port ' + port);
         AnyFetch.setManagerUrl('http://localhost:' + port);
-        done();
-      });
-    });
-
-    it('should err on invalid appId', function(done) {
-      AnyFetch.getAccessToken('wrong_app_id', appSecret, code, function(err) {
-        should(err).have.property('message').and.match(/404/);
-        done();
-      });
-    });
-
-    it('should err on invalid appSecret', function(done) {
-      AnyFetch.getAccessToken(appId, 'wrong_app_secret', code, function(err) {
-        should(err).have.property('message').and.match(/401/);
         done();
       });
     });
@@ -55,7 +40,7 @@ describe('<Auth>', function() {
     });
 
     after(function closeServer() {
-      fakeManagerServer.close();
+      mockServer.close();
     });
   });
 });

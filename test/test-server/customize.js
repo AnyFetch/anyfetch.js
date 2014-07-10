9439c8e4f87daef118edefb65f4cadeec0da9396
Merlin Nimier-David <merlin.nimier@gmail.com>
2014-07-10 16:49:19 +0200

    English

--- test/test-server/customize.js
+++ test/test-server/customize.js
@@ -9,7 +9,7 @@ var configuration = require('../../config/configuration.js');
 
 describe('<Mock server customization>', function() {
   var endpoint = '/status';
-  var overridedContent = require('../samples/mock.json');
+  var overridenContent = require('../samples/mock.json');
 
   var anyfetch = new AnyFetch('email', 'password');
 
@@ -33,10 +33,10 @@ describe('<Mock server customization>', function() {
     server.restore();
   });
 
-  var checkOverrided = function(expected, done) {
+  var checkoverriden = function(expected, done) {
     if(!done) {
       done = expected;
-      expected = overridedContent;
+      expected = overridenContent;
     }
 
     mockRequest.get(endpoint)
@@ -46,30 +46,30 @@ describe('<Mock server customization>', function() {
   };
 
   describe('Overriding', function() {
-    it('should serve overrided JSON', function(done) {
-      server.override('get', endpoint, overridedContent);
-      checkOverrided(done);
+    it('should serve overriden JSON', function(done) {
+      server.override('get', endpoint, overridenContent);
+      checkoverriden(done);
     });
 
-    it('should serve overrided JSON from filename', function(done) {
+    it('should serve overriden JSON from filename', function(done) {
       var filename = __dirname + '/../samples/mock.json';
       server.override('get', endpoint, filename);
-      checkOverrided(done);
+      checkoverriden(done);
     });
 
     it('should override GET by default', function(done) {
       var filename = __dirname + '/../samples/mock.json';
       server.override(endpoint, filename);
-      checkOverrided(done);
+      checkoverriden(done);
     });
 
-    it('should ignore querystring of endpoint', function(done) {
-      server.override(endpoint + '?useful=false&nonsense[0]=true', overridedContent);
-      checkOverrided(done);
+    it('should ignore endpoint querystring', function(done) {
+      server.override(endpoint + '?useful=false&nonsense[0]=true', overridenContent);
+      checkoverriden(done);
     });
 
     it('should be able to override /oauth/access_token as well', function(done) {
-      server.override('post', '/oauth/access_token', overridedContent);
+      server.override('post', '/oauth/access_token', overridenContent);
 
       var data = {
         client_id: 'chuck_norris',
@@ -80,27 +80,27 @@ describe('<Mock server customization>', function() {
       mockRequest.post('/oauth/access_token')
         .send(data)
         .expect(200)
-        .expect(overridedContent)
+        .expect(overridenContent)
         .end(done);
     });
   });
 
   describe('Restoring', function() {
-    it('should restore a single endpoints', function(done) {
-      server.override(endpoint, overridedContent);
+    it('should restore a single endpoint', function(done) {
+      server.override(endpoint, overridenContent);
       server.restore(endpoint);
       mockRequest.get(endpoint)
         .expect(200)
         .expect(function(res) {
           should(res.body).be.ok;
-          res.body.should.not.have.properties(overridedContent);
+          res.body.should.not.have.properties(overridenContent);
         })
         .end(done);
     });
 
     it('should restore all endpoints', function(done) {
-      server.override('/status', overridedContent);
-      server.override('/providers', overridedContent);
+      server.override('/status', overridenContent);
+      server.override('/providers', overridenContent);
       server.restore();
 
       async.parallel({
@@ -109,7 +109,7 @@ describe('<Mock server customization>', function() {
             .expect(200)
             .expect(function(res) {
               should(res.body).be.ok;
-              res.body.should.not.have.properties(overridedContent);
+              res.body.should.not.have.properties(overridenContent);
             })
             .end(cb);
         },
@@ -118,7 +118,7 @@ describe('<Mock server customization>', function() {
             .expect(200)
             .expect(function(res) {
               should(res.body).be.ok;
-              res.body.should.not.have.properties(overridedContent);
+              res.body.should.not.have.properties(overridenContent);
             })
             .end(cb);
         }
@@ -129,7 +129,7 @@ describe('<Mock server customization>', function() {
   describe('Edge cases', function() {
     it('should err when overriding GET /batch', function() {
       try {
-        server.override('/batch', overridedContent);
+        server.override('/batch', overridenContent);
       } catch(e) {
         should(e).be.ok;
         e.message.should.match(/cannot override \/batch/i);
@@ -138,7 +138,7 @@ describe('<Mock server customization>', function() {
 
     it('should err when overriding unknown HTTP verb', function() {
       try {
-        server.override('PONEY', '/batch', overridedContent);
+        server.override('PONY', '/batch', overridenContent);
       } catch(e) {
         should(e).be.ok;
         e.message.should.match(/unknown http verb/i);
@@ -146,7 +146,7 @@ describe('<Mock server customization>', function() {
     });
 
     it('should still serve no content on 204 endpoints', function(done) {
-      server.override('delete', '/token', overridedContent);
+      server.override('delete', '/token', overridenContent);
       mockRequest.delete('/token')
         .expect(204)
         .expect(function(res) {

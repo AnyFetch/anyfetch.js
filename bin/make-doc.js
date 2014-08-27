1f7b94c891661cf633c15ddc169f0bdf5fe1380d
Merlin Nimier-David <merlin.nimier@gmail.com>
2014-08-27 17:06:31 +0200

    Script to generate a reference documentation for mapping functions

---
+++ bin/make-doc.js
@@ -0,0 +1,111 @@
+'use strict';
+
+/**
+ * Usage: `node bin/make-doc.js`
+ * Use this file to generate a reference sheet listing all mapping functions
+ * with their arguments.
+ *
+ * @warning When a function is aliased, we show only the aliased name.
+ */
+
+var fs = require('fs');
+var config = require('../config/configuration.js');
+
+var outputFilename = 'reference.md';
+var header = 'Mappings\n========\n';
+header += 'These functions are the easiest way to request the AnyFetch API. Each function corresponds directly to an endpoint: **please see the [full API reference](http://developers.anyfetch.com/endpoints/)**.\n\n';
+header += 'Most callbacks are expected to be of the form: `function cb(err, res)`, where `res` is a [SuperAgent Response object](http://visionmedia.github.io/superagent/#response-properties) directly representing the reponse obtained from the API.\n';
+
+var applyAliases = function(descriptors, aliases) {
+  Object.keys(aliases).forEach(function(alias) {
+    var original = aliases[alias];
+    descriptors[alias] = descriptors[original];
+    delete descriptors[original];
+  });
+}
+
+var groupByEndpoint = function(descriptors) {
+  var grouped = {};
+
+  Object.keys(descriptors).forEach(function(functionName) {
+    var endpoint = descriptors[functionName].endpoint;
+    if(!grouped[endpoint]) {
+      grouped[endpoint] = {};
+    }
+
+    grouped[endpoint][functionName] = descriptors[functionName];
+  });
+
+  return grouped;
+};
+
+var generateSignature = function(functionName, descriptor) {
+  var args = [];
+  var details = '';
+  if(descriptor.requireId) {
+    args.push('id');
+    details += '\n  - `id` (string): a valid MongoDB ObjectId';
+  }
+  if(descriptor.requireIdentifier) {
+    args.push('identifier');
+    details += '\n  - `identifier` (string): the custom identifier set for your document';
+  }
+  if(descriptor.params) {
+    args.push('[params]');
+    var params = descriptor.params.map(function(param){
+      return '`' + param + '`';
+    });
+    details += '\n  - `params` (object): will be passed as GET parameters.';
+    details += '\nSupported keys: ' + params.join(', ');
+  }
+  if(descriptor.body) {
+    args.push('[body]');
+    var body = descriptor.body.map(function(key){
+      return '`' + key + '`';
+    });
+
+    details += '\n  - `body` (object): will be sent as the request\'s body (POST).';
+    details += '\nSupported keys: ' + body.join(', ');
+  }
+  details += '\n';
+  args.push('cb');
+
+  return functionName + '(' + args.join(', ') + ')`\n' + details;
+}
+
+var generateBody = function(descriptorsByEndpoint) {
+  var body = '';
+
+  for(var endpoint in descriptorsByEndpoint) {
+    var descriptors = descriptorsByEndpoint[endpoint];
+
+    body += '\n### `' + endpoint + '` endpoint\n\n';
+
+    for(var functionName in descriptors) {
+      var descriptor = descriptors[functionName];
+      body += '- `' + generateSignature(functionName, descriptor);
+      // TODO: subfunctions
+    }
+  }
+
+  return body;
+}
+
+var writeMarkdown = function(content) {
+  fs.writeFileSync(outputFilename, content);
+};
+
+var make = function() {
+  applyAliases(config.apiDescriptors, config.aliases);
+  var grouped = groupByEndpoint(config.apiDescriptors);
+
+  var body = '## Mapping functions ordered by API endpoint';
+  body += generateBody(grouped);
+
+  writeMarkdown(header + body);
+
+  console.log('Reference documentation written to ' + outputFilename);
+  process.exit(0);
+}
+
+make();
